740bc88799227cb55168aaa74e43371b36f6a6e1
wrobson5467 <will.robson@gmail.com>
2022-05-07 12:41:05 -0700

    first commit for backend

---
+++ server/models/userModels.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// User's information is stored on Mongo DB Atlas
+const mongoURI = 'mongodb+srv://swags:parksandrec@cluster0.n5ovd.mongodb.net/parksandrecusers?retryWrites=true&w=majority'
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    dbName: 'parksandrecusers'
+})
+
+.then(() => console.log('Connected to Parks and Rec Users DB.'))
+.catch(err => console.log(err));
+
+
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcrypt')
+
+
+const userSchema = new Schema ({
+    userID: Number,
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    trips: []
+})
+
+
+module.exports = mongoose.model('User', userSchema);
